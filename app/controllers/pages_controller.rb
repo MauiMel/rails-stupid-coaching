0508a1b23dcc4a64f0cbc8c4a1797a1e2d2ac59d
Melissa Connors <melconnors04@yahoo.com>
2019-08-12 21:31:26 +0100

    fixed some of the q&a code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,17 @@
 class PagesController < ApplicationController
   def ask
-    @ask = 'Hello there'
   end
 
   def answer
-    @question = params[:ask_question]
-      if @question = 'Can I go shopping?'
-        return @answer = 'Great!'
-      elsif @question.ends_with? '?'
-        return @answer = 'Silly question, get dressed and go to work!'
-      else @answer = 'I dont care, get dressed and go to work!'
+    @question = params[:question]
+      if @question.blank?
+        @answer = "I can't hear you!"
+      elsif @question =~ /i am going to work/i
+        @answer = 'Great!'
+      elsif @question.ends_with?('?')
+        @answer = 'Silly question, get dressed and go to work!'
+      else
+        @answer = "I don't care, get dressed and go to work!"
       end
   end
 end
